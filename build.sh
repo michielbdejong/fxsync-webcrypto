fb9badac45bb4ba5b845c42a4884a504bbd5e94e
Michiel de Jong <mbdejong@mozilla.com>
2015-08-17 15:10:11 +0200

    exclude dist/ from repo

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 jshint src/ test/
+mkdir -p dist/
 echo "(function(window) {" > dist/fxsync-webcrypto.js
 cat src/string-conversion.js src/key-derivation.js src/main.js >> dist/fxsync-webcrypto.js
 echo "})(window);" >> dist/fxsync-webcrypto.js
