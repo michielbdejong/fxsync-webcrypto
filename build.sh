0720706ce8765f64457d642375f293d7fb394f89
Michiel de Jong <mbdejong@mozilla.com>
2015-08-17 12:53:07 +0200

    Rename utils.js -> string-conversion.js

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 echo "(function(window) {" > dist/fxsync-webcrypto.js
-cat src/utils.js src/hkdf.js src/main.js >> dist/fxsync-webcrypto.js
+cat src/string-conversion.js src/hkdf.js src/main.js >> dist/fxsync-webcrypto.js
 echo "})(window);" >> dist/fxsync-webcrypto.js
